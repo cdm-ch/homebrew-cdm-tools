7e042c57fa9d11792fe296b79c52ece799e4ce39
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-27 21:52:55 +0200

    Update version-compare to 1.0.1

--- Formula/version-compare.rb
+++ Formula/version-compare.rb
@@ -1,45 +1,23 @@
+# Homebrew Formula snippet for version-compare v1.0.1
+# Copy this into your Formula/version-compare.rb file
+
 class VersionCompare < Formula
-  desc "Concurrent Kubernetes cluster version comparison tool written in Go"
+  desc "Concurrent Kubernetes cluster version comparison tool"
   homepage "https://git.cdm.smis.ch/bitbucket/scm/ccpt/homebrew-cdm-tools"
-  version "1.0.0"
-  license "Proprietary"
-
-  # Nexus URL - anonymous read access enabled
-  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.0/version-compare-1.0.0-darwin-arm64.tar.gz"
-  sha256 "267bc3fc01cd9dd43b3a52171a9fd4151e2d1cc83a0eca3d637a2dc136183787"
-
-  # This tool requires ARM64 Macs
+  version "1.0.1"
+  
+  # Anonymous read access enabled in Nexus
+  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.1/version-compare-1.0.1-darwin-arm64.tar.gz"
+  
+  sha256 "9238dbcd6a083ddcf23efcc370b194e4dcb200abb29eef5343683c1ac7b380d9"
+  
   depends_on arch: :arm64
-
+  
   def install
     bin.install "version-compare"
   end
-
-  def caveats
-    <<~EOS
-      version-compare has been installed!
-      
-      Usage:
-        version-compare
-      
-      The tool will interactively guide you through:
-        - Selecting two Kubernetes clusters to compare
-        - Choosing namespace filters (cdm, ccp, cpm, cpt, all)
-        - Selecting resource types (pods, deployments, jobs, cronjobs)
-        - Choosing output mode (simple or detailed)
-      
-      Requirements:
-        - Valid kubeconfig at ~/.kube/config
-        - Access to the Kubernetes clusters you want to compare
-      
-      To upgrade to the latest version:
-        brew update && brew upgrade version-compare
-    EOS
-  end
-
+  
   test do
-    # Basic test to ensure binary runs
-    assert_match "Kubernetes Version Compare Tool", 
-                 shell_output("#{bin}/version-compare --help 2>&1", 1)
+    assert_match "Version Compare Tool", shell_output("#{bin}/version-compare --help 2>&1", 1)
   end
 end
