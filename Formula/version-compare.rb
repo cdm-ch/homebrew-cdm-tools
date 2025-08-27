5b036f249ea5060893ccd3bc3a82eb75108a9590
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-27 22:12:47 +0200

    Update version-compare to 1.0.3

--- Formula/version-compare.rb
+++ Formula/version-compare.rb
@@ -1,15 +1,15 @@
-# Homebrew Formula snippet for version-compare v1.0.2
+# Homebrew Formula snippet for version-compare v1.0.3
 # Copy this into your Formula/version-compare.rb file
 
 class VersionCompare < Formula
   desc "Concurrent Kubernetes cluster version comparison tool"
   homepage "https://git.cdm.smis.ch/bitbucket/scm/ccpt/homebrew-cdm-tools"
-  version "1.0.2"
+  version "1.0.3"
   
   # Anonymous read access enabled in Nexus
-  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.2/version-compare-1.0.2-darwin-arm64.tar.gz"
+  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.3/version-compare-1.0.3-darwin-arm64.tar.gz"
   
-  sha256 "f5849a2b9398c49769c297b5d06a30ae68c5d45bad54265fa61b2c747b9e977a"
+  sha256 "510d26deccf4e0a557892b5b40d9dff987eef3c64b9e73597c3df8991bcdb432"
   
   depends_on arch: :arm64
   
