e7d593b8dee2174010431a1ebc95842140332199
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-28 00:17:56 +0200

    Add better-clone 1.0.1

--- Formula/better-clone.rb
+++ Formula/better-clone.rb
@@ -1,14 +1,14 @@
-# Homebrew Formula for better-clone v1.0.0
+# Homebrew Formula for better-clone v1.0.1
 
 class BetterClone < Formula
   desc "Concurrent Bitbucket repository cloner with interactive project selection"
   homepage "https://git.cdm.smis.ch/bitbucket/scm/ccpt/convenience-scripts"
-  version "1.0.0"
+  version "1.0.1"
   
   # Anonymous read access enabled in Nexus
-  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.0/better-clone-1.0.0-darwin-arm64.tar.gz"
+  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.1/better-clone-1.0.1-darwin-arm64.tar.gz"
   
-  sha256 "07cb28c5807baa6957eaf276e795d33a27937bf77858bbcb6caf4d7dd51d333d"
+  sha256 "6fdf35c99118a4b4c0c11b546f3127ae0c12779e73cd7abf2198952c67c30ab2"
   
   depends_on arch: :arm64
   
