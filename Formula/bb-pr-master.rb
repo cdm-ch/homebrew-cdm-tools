344a1152b008e00c7101104846d29643db7e7a0f
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 10:09:31 +0200

    Update bb-pr-master to version 1.0.1

--- Formula/bb-pr-master.rb
+++ Formula/bb-pr-master.rb
@@ -1,22 +1,23 @@
-# Homebrew Formula for bb-pr-master v1.0.0
+class ub_pr_master < Formula
+  desc "CDM tool: bb-pr-master"
+  homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
+  version "1.0.1"
+  
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.1/bb-pr-master-1.0.1-darwin-intel.tar.gz"
+      sha256 ""
+    else
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.1/bb-pr-master-1.0.1-darwin-arm.tar.gz"
+      sha256 ""
+    end
+  end
 
-class BbPrMaster < Formula
-  desc "Bitbucket Pull Request CLI tool for batch operations across repositories"
-  homepage "https://git.cdm.smis.ch/bitbucket/scm/ccpt/ccp-go-tools"
-  version "1.0.0"
-  
-  # Anonymous read access enabled in Nexus
-  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.0/bb-pr-master-1.0.0-darwin-arm64.tar.gz"
-  
-  sha256 "d720681f01c5ccffa5a83643afaae5804b9be375f30e954eeacfd889c2242d05"
-  
-  depends_on arch: :arm64
-  
   def install
     bin.install "bb-pr-master"
   end
-  
+
   test do
-    assert_match "bb-pr-master", shell_output("#{bin}/bb-pr-master --help 2>&1")
+    system "#{bin}/bb-pr-master", "--version"
   end
 end
