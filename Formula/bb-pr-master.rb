11b646b69c910e391f4bbc448c80d5dbd3b77d38
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 13:31:45 +0200

    Update bb-pr-master to version 1.0.9

--- Formula/bb-pr-master.rb
+++ Formula/bb-pr-master.rb
@@ -1,15 +1,15 @@
 class BbPrMaster < Formula
   desc "CDM tool: bb-pr-master"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.8"
+  version "1.0.9"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.8/bb-pr-master-1.0.8-darwin-intel.tar.gz"
-      sha256 "a07e0f35badeee405ba5160c4e783c32eeb9e9866b6fe9800fd28cd37962fe05"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.9/bb-pr-master-1.0.9-darwin-intel.tar.gz"
+      sha256 "f42a1dd2b956d41263a4c17c5548d0f7fbac93eb8408de9724e98fd1141b8c52"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.8/bb-pr-master-1.0.8-darwin-arm.tar.gz"
-      sha256 "82ca0fe42e9ad09813e76eb06ea1bfe041229cad5137b4a67ef955b0b7490fde"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.9/bb-pr-master-1.0.9-darwin-arm.tar.gz"
+      sha256 "fb7d373181e32b6c2b96171c413ac239957b03c9ac2fbfa473f75871684fb963"
     end
   end
 
