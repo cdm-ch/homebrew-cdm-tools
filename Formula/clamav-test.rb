93fc9508a41bca6f936b5287065648a66fe277cc
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 10:10:52 +0200

    Update clamav-test to version 1.0.0

--- Formula/clamav-test.rb
+++ Formula/clamav-test.rb
@@ -1,14 +1,14 @@
 class clamav_test < Formula
   desc "CDM tool: clamav-test"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "0.1.1"
+  version "1.0.0"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/0.1.1/clamav-test-0.1.1-darwin-intel.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.0.0/clamav-test-1.0.0-darwin-intel.tar.gz"
       sha256 ""
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/0.1.1/clamav-test-0.1.1-darwin-arm.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.0.0/clamav-test-1.0.0-darwin-arm.tar.gz"
       sha256 ""
     end
   end
