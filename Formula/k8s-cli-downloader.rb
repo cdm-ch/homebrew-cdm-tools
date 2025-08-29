f79a2336a90fd769fb2185dc433b61a37553847d
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 10:11:16 +0200

    Update k8s-cli-downloader to version 1.0.3

--- Formula/k8s-cli-downloader.rb
+++ Formula/k8s-cli-downloader.rb
@@ -1,22 +1,23 @@
-# Homebrew Formula for k8s-cli-downloader v1.0.0
+class k8s_cli_downloader < Formula
+  desc "CDM tool: k8s-cli-downloader"
+  homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
+  version "1.0.3"
+  
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.3/k8s-cli-downloader-1.0.3-darwin-intel.tar.gz"
+      sha256 ""
+    else
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.3/k8s-cli-downloader-1.0.3-darwin-arm.tar.gz"
+      sha256 ""
+    end
+  end
 
-class K8sCliDownloader < Formula
-  desc "Unified downloader for Kubernetes CLI tools (kubectl, flux, istioctl)"
-  homepage "https://git.cdm.smis.ch/bitbucket/scm/ccpt/convenience-scripts"
-  version "1.0.0"
-  
-  # Anonymous read access enabled in Nexus
-  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.0/k8s-cli-downloader-1.0.0-darwin-arm64.tar.gz"
-  
-  sha256 "60f82a0edb1c4e2b6148eeb340c20f3028580bfe05329ef63b50ab96b98df09b"
-  
-  depends_on arch: :arm64
-  
   def install
     bin.install "k8s-cli-downloader"
   end
-  
+
   test do
-    assert_match "kubectl", shell_output("#{bin}/k8s-cli-downloader list kubectl --limit 1")
+    system "#{bin}/k8s-cli-downloader", "--version"
   end
 end
