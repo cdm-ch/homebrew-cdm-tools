ecbd11a22907311fb90f9e08e1dcd5c87586d929
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 10:10:01 +0200

    Update better-clone to version 1.0.2

--- Formula/better-clone.rb
+++ Formula/better-clone.rb
@@ -1,22 +1,23 @@
-# Homebrew Formula for better-clone v1.0.1
+class uetter_clone < Formula
+  desc "CDM tool: better-clone"
+  homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
+  version "1.0.2"
+  
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.2/better-clone-1.0.2-darwin-intel.tar.gz"
+      sha256 ""
+    else
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.2/better-clone-1.0.2-darwin-arm.tar.gz"
+      sha256 ""
+    end
+  end
 
-class BetterClone < Formula
-  desc "Concurrent Bitbucket repository cloner with interactive project selection"
-  homepage "https://git.cdm.smis.ch/bitbucket/scm/ccpt/convenience-scripts"
-  version "1.0.1"
-  
-  # Anonymous read access enabled in Nexus
-  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.1/better-clone-1.0.1-darwin-arm64.tar.gz"
-  
-  sha256 "6fdf35c99118a4b4c0c11b546f3127ae0c12779e73cd7abf2198952c67c30ab2"
-  
-  depends_on arch: :arm64
-  
   def install
     bin.install "better-clone"
   end
-  
+
   test do
-    assert_match "better-clone", shell_output("#{bin}/better-clone --help 2>&1")
+    system "#{bin}/better-clone", "--version"
   end
 end
