d82215c93a868cabf58a25121fcc5a5bcf02cdc3
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 11:32:49 +0200

    Update better-clone to version 1.0.6

--- Formula/better-clone.rb
+++ Formula/better-clone.rb
@@ -1,14 +1,14 @@
 class BetterClone < Formula
   desc "CDM tool: better-clone"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.5"
+  version "1.0.6"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.5/better-clone-1.0.5-darwin-intel.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.6/better-clone-1.0.6-darwin-intel.tar.gz"
       sha256 ""
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.5/better-clone-1.0.5-darwin-arm.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.0.6/better-clone-1.0.6-darwin-arm.tar.gz"
       sha256 ""
     end
   end
