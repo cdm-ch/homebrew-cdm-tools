5994bd56c7af609e9b8050a08c960d3b41de356b
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 12:30:56 +0200

    Update release-helper to version 1.0.3

--- Formula/release-helper.rb
+++ Formula/release-helper.rb
@@ -1,15 +1,15 @@
 class ReleaseHelper < Formula
   desc "CDM tool: release-helper"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.2"
+  version "1.0.3"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/release-helper/1.0.2/release-helper-1.0.2-darwin-intel.tar.gz"
-      sha256 "960491c2964d066e671ee101d7a7ee4fc1a79b81dfc0135395c3cd83a98fe3c4"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/release-helper/1.0.3/release-helper-1.0.3-darwin-intel.tar.gz"
+      sha256 "aa46904fac76af24dd1e04c862f79f7ab663dd3de105c492756047a6f125c853"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/release-helper/1.0.2/release-helper-1.0.2-darwin-arm.tar.gz"
-      sha256 "1f41d0177e5fbbe4b1bc86b34d88bb0331de27d0dba4ad0beb21e8b332f17ba1"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/release-helper/1.0.3/release-helper-1.0.3-darwin-arm.tar.gz"
+      sha256 "ea3dc250fb6a711756d10e02f574bc5fb9e4dbd5d822965992956030e48315f5"
     end
   end
 
