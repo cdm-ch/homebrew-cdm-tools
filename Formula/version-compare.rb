1fdf5d9744e5f11ce4a0c9838b0d6a1cf8ea4c32
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 10:11:54 +0200

    Update version-compare to version 1.0.8

--- Formula/version-compare.rb
+++ Formula/version-compare.rb
@@ -1,23 +1,23 @@
-# Homebrew Formula snippet for version-compare v1.0.7
-# Copy this into your Formula/version-compare.rb file
+class version_compare < Formula
+  desc "CDM tool: version-compare"
+  homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
+  version "1.0.8"
+  
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.8/version-compare-1.0.8-darwin-intel.tar.gz"
+      sha256 ""
+    else
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.8/version-compare-1.0.8-darwin-arm.tar.gz"
+      sha256 ""
+    end
+  end
 
-class VersionCompare < Formula
-  desc "Concurrent Kubernetes cluster version comparison tool"
-  homepage "https://git.cdm.smis.ch/bitbucket/scm/ccpt/homebrew-cdm-tools"
-  version "1.0.7"
-  
-  # Anonymous read access enabled in Nexus
-  url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.7/version-compare-1.0.7-darwin-arm64.tar.gz"
-  
-  sha256 "994aade8fccc016aa69265444e14988dcabe2c02cf7522e9215ebadae9a964aa"
-  
-  depends_on arch: :arm64
-  
   def install
     bin.install "version-compare"
   end
-  
+
   test do
-    assert_match "Version Compare Tool", shell_output("#{bin}/version-compare --help 2>&1", 1)
+    system "#{bin}/version-compare", "--version"
   end
 end
