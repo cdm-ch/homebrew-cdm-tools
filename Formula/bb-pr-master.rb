d61aae6dd852db006ae090717f6e7ec272e3ac5f
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 10:20:25 +0200

    Update bb-pr-master to version 1.0.3

--- Formula/bb-pr-master.rb
+++ Formula/bb-pr-master.rb
@@ -1,14 +1,14 @@
 class BbPrMaster < Formula
   desc "CDM tool: bb-pr-master"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.2"
+  version "1.0.3"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.2/bb-pr-master-1.0.2-darwin-intel.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.3/bb-pr-master-1.0.3-darwin-intel.tar.gz"
       sha256 ""
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.2/bb-pr-master-1.0.2-darwin-arm.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-pr-master/1.0.3/bb-pr-master-1.0.3-darwin-arm.tar.gz"
       sha256 ""
     end
   end
