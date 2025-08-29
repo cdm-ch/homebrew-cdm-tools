7c8f399dbe7cbf845d2bcb1e6677636ccd2c1b7b
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 13:31:52 +0200

    Update clamav-test to version 1.0.8

--- Formula/clamav-test.rb
+++ Formula/clamav-test.rb
@@ -1,15 +1,15 @@
 class ClamavTest < Formula
   desc "CDM tool: clamav-test"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.7"
+  version "1.0.8"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.0.7/clamav-test-1.0.7-darwin-intel.tar.gz"
-      sha256 "f5e2b28faefb034d9e334384eb74547b41f01540e319c4d526c6227cafa172d3"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.0.8/clamav-test-1.0.8-darwin-intel.tar.gz"
+      sha256 "1e07bd85887bdd7c26d10a589d3f8bca7fea790956a76530301784cf5b79438e"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.0.7/clamav-test-1.0.7-darwin-arm.tar.gz"
-      sha256 "604e2a214882299d69bc5bae80690b5e8e9a977e1ab0f69ad1a5d201ea5ee777"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.0.8/clamav-test-1.0.8-darwin-arm.tar.gz"
+      sha256 "63451be289a78bdc54605cee14c90274f01e594ee095b07643ee883c83c12a1b"
     end
   end
 
