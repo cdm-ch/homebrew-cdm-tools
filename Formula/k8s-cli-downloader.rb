4e230744bcd972301c0fbd323f5f3127510da10d
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-08-29 10:20:33 +0200

    Update k8s-cli-downloader to version 1.0.5

--- Formula/k8s-cli-downloader.rb
+++ Formula/k8s-cli-downloader.rb
@@ -1,14 +1,14 @@
 class K8sCliDownloader < Formula
   desc "CDM tool: k8s-cli-downloader"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.4"
+  version "1.0.5"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.4/k8s-cli-downloader-1.0.4-darwin-intel.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.5/k8s-cli-downloader-1.0.5-darwin-intel.tar.gz"
       sha256 ""
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.4/k8s-cli-downloader-1.0.4-darwin-arm.tar.gz"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.5/k8s-cli-downloader-1.0.5-darwin-arm.tar.gz"
       sha256 ""
     end
   end
