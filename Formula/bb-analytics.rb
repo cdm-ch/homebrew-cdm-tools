b23b43feb07ed043c55a78d022efd48278e0d2b6
MOOR, Arne <arne.moor@aerztekasse.ch>
2025-10-03 09:54:14 +0200

    Update bb-analytics to version 1.0.2

--- Formula/bb-analytics.rb
+++ Formula/bb-analytics.rb
@@ -1,15 +1,15 @@
 class BbAnalytics < Formula
   desc "CDM tool: bb-analytics"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.1"
+  version "1.0.2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-analytics/1.0.1/bb-analytics-1.0.1-darwin-intel.tar.gz"
-      sha256 "499df014e63135dd4b9a5af363326e41a6fbef527457df217949d3ed078eb9a5"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-analytics/1.0.2/bb-analytics-1.0.2-darwin-intel.tar.gz"
+      sha256 "b2337354e6168ea0978816f80328b03d87d763959f35da096b5f4185c3c2b3c8"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-analytics/1.0.1/bb-analytics-1.0.1-darwin-arm.tar.gz"
-      sha256 "0e83aa7e9b2c4fc2601c53b0629582f721ab086cc76e5df462ac23356bddb882"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/bb-analytics/1.0.2/bb-analytics-1.0.2-darwin-arm.tar.gz"
+      sha256 "9b756e0cf438729b6ed04fc3fb32387050c4e12eab57a9ea9909fe773ce6e0ac"
     end
   end
 
