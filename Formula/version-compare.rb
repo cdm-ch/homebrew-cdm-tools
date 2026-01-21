bb63396ec9026b59bc8cc62ec8d783157e7c75fc
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-01-21 21:11:29 +0100

    Update version-compare to version 1.0.17

--- Formula/version-compare.rb
+++ Formula/version-compare.rb
@@ -1,15 +1,15 @@
 class VersionCompare < Formula
   desc "CDM tool: version-compare"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.16"
+  version "1.0.17"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.16/version-compare-1.0.16-darwin-intel.tar.gz"
-      sha256 "881ca808a57ecaff9352d1108413ee60daff6bfb0266904c6b5527fd7d0ab4fa"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.17/version-compare-1.0.17-darwin-intel.tar.gz"
+      sha256 "215bf21f084199d524598cd56bd2ca02a5eb3172a9e7d0c3a5b75686c11f461b"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.16/version-compare-1.0.16-darwin-arm.tar.gz"
-      sha256 "6b4dcbb85ec3e39a0afe938b042d2b098ce6ccb6dad609957298e540b9b4e692"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.0.17/version-compare-1.0.17-darwin-arm.tar.gz"
+      sha256 "412550082228ff75a7ad7aa551cfe0ba76a52acd82dc2392e3b7c0bd31b89d9f"
     end
   end
 
