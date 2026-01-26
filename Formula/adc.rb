119f4ab16e4c9eb12baf1b7028f3cf779754ba56
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-01-26 17:39:36 +0100

    Update adc to version 1.4.7

--- Formula/adc.rb
+++ Formula/adc.rb
@@ -1,15 +1,15 @@
 class Adc < Formula
   desc "Atlassian Data Center CLI for Jira and Bitbucket"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.4.6"
+  version "1.4.7"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.4.6/adc-1.4.6-darwin-intel.tar.gz"
-      sha256 "35ce44727bef65c605f3560f460f48ee776bfbad02f76df59a2a43cf2917d7ce"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.4.7/adc-1.4.7-darwin-intel.tar.gz"
+      sha256 "c2aa8e88f4a9f602fad9da1dab6532be5436b2e6a6535f6dc13905eb5270d5cf"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.4.6/adc-1.4.6-darwin-arm.tar.gz"
-      sha256 "645c49252599c95552e2d6e7748ca20e8607bc055c0347ba9dd39cc909ccf935"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.4.7/adc-1.4.7-darwin-arm.tar.gz"
+      sha256 "5ba2b752b3db82691bb9707c03719294a628a7f048e53806fe26a51e067f4619"
     end
   end
 
