3acaf557f8684d706ddf47feb60b048ec20bb717
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-02-07 11:19:47 +0100

    Update better-clone to version 1.1.1

--- Formula/better-clone.rb
+++ Formula/better-clone.rb
@@ -1,15 +1,15 @@
 class BetterClone < Formula
   desc "CDM tool: better-clone"
   homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.1.0"
+  version "1.1.1"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.1.0/better-clone-1.1.0-darwin-intel.tar.gz"
-      sha256 "05b75e437221f6b1ae66d276bcad2c8228963ce403108451abeb8bb67c8c2864"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.1.1/better-clone-1.1.1-darwin-intel.tar.gz"
+      sha256 "bc5ad978e021a5095b356dee4df1c790e85be85ed65dabe3b872485b3c23e040"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.1.0/better-clone-1.1.0-darwin-arm.tar.gz"
-      sha256 "99cc42abceef607f5999fc172a699a65d9868ab66684e71477dcfb101d6e79f5"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/better-clone/1.1.1/better-clone-1.1.1-darwin-arm.tar.gz"
+      sha256 "adba1d529c3e8e7d2efddb0513e0dad48e2c25f08291c249e9a8f2ba25bbc4b7"
     end
   end
 
