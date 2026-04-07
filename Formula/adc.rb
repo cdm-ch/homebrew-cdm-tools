2fca420ce0635d89c60ad639172324eca1c42076
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-04-07 19:35:44 +0200

    Update adc to version 1.6.0

--- Formula/adc.rb
+++ Formula/adc.rb
@@ -6,10 +6,10 @@ class Adc < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.6.0/adc-1.6.0-darwin-intel.tar.gz"
-      sha256 "dc645608d275143bf98910c050df76857a28e18f46fb2150f7afb1811ba64b3a"
+      sha256 "c9cef71681e74ff88ff9f1787de50773f6d82a85db1d2908dd13f839f3fcae73"
     else
       url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.6.0/adc-1.6.0-darwin-arm.tar.gz"
-      sha256 "143301301ca5cca698cd3f9596d558ee5e2c7e63314644a5f603146e5e5d97f5"
+      sha256 "10c4a65720cb241138b9020852f5bf27b12615635b52b2165e5376aee2c084a3"
     end
   end
 
