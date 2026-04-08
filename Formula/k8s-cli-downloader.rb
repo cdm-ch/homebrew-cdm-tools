8c43259a0c5a8d09d2b16276475a4dbe87afcda7
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-04-08 17:57:07 +0200

    Update k8s-cli-downloader to version 1.1.0

--- Formula/k8s-cli-downloader.rb
+++ Formula/k8s-cli-downloader.rb
@@ -1,15 +1,15 @@
 class K8sCliDownloader < Formula
   desc "CDM tool: k8s-cli-downloader"
-  homepage "https://git.cdm.smis.ch/bitbucket/projects/CCPT"
-  version "1.0.15"
+  homepage "https://github.com/cdm-ch"
+  version "1.1.0"
   
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.15/k8s-cli-downloader-1.0.15-darwin-intel.tar.gz"
-      sha256 "e184132bf93305ea908d89bc224de24abe96e0eb7def5e70105e85a836fed350"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.1.0/k8s-cli-downloader-1.1.0-darwin-intel.tar.gz"
+      sha256 "4c2bc616e4afeba2cf2dbb7c39b3a27f71161bf2b04fe2c3407cbf221aaf6589"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.0.15/k8s-cli-downloader-1.0.15-darwin-arm.tar.gz"
-      sha256 "1926ddc22f43f3d3a983cb00e8056c0bea26894c04a042d2b2fe572f382e6b5d"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/k8s-cli-downloader/1.1.0/k8s-cli-downloader-1.1.0-darwin-arm.tar.gz"
+      sha256 "00dacf61e02bec29425819e06e96a84f1165acdcb16902be68cec4643dc096ce"
     end
   end
 
