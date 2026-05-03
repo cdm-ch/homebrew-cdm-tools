ce2ce74f0931b5ea96113c2a7aed55c5837b90ee
Arne Moor <arm@dizh911.local>
2026-05-03 15:07:25 +0200

    Update adc to version 1.11.4

--- Formula/adc.rb
+++ Formula/adc.rb
@@ -1,15 +1,15 @@
 class Adc < Formula
   desc "Atlassian Data Center CLI for Jira and Bitbucket"
   homepage "https://github.com/cdm-ch"
-  version "1.11.3"
+  version "1.11.4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.11.3/adc-1.11.3-darwin-intel.tar.gz"
-      sha256 "2b9f41d546bdc7ba6ef1847862486697b1d79bf63cce3a65ea660bb4b6e47c08"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.11.4/adc-1.11.4-darwin-intel.tar.gz"
+      sha256 "8dd3e61638cdf55135cd235bedfee3d1b5697f009f06b940e9ad688ce4136396"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.11.3/adc-1.11.3-darwin-arm.tar.gz"
-      sha256 "a6881039cc59a4bcd584a14f01438c3e194218ce8cd286db9efb34473282b36e"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.11.4/adc-1.11.4-darwin-arm.tar.gz"
+      sha256 "2893847feade1d165fb4f37e482f4470fe92807f8c973200241eccc0a31378a5"
     end
   end
 
