bc642232bc6de38fbfb79f1a846689706b24aeb1
Arne Moor <arm@dizh911.local>
2026-06-06 11:02:59 +0200

    Update clamav-test to version 1.3.0

--- Formula/clamav-test.rb
+++ Formula/clamav-test.rb
@@ -1,15 +1,15 @@
 class ClamavTest < Formula
   desc "CDM tool: clamav-test"
   homepage "https://github.com/cdm-ch"
-  version "1.2.0"
-  
+  version "1.3.0"
+
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.2.0/clamav-test-1.2.0-darwin-intel.tar.gz"
-      sha256 "5c35053c396783d709d81bbcdd4a8d6a4ee79bec3ca539f2aea7d77aaff63990"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.3.0/clamav-test-1.3.0-darwin-intel.tar.gz"
+      sha256 "fe676abf2e42cb7abf4cd67c24712e356d4c068764da36c9d79964b06bda1243"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.2.0/clamav-test-1.2.0-darwin-arm.tar.gz"
-      sha256 "c8b6d4a91d95f7af264d7da15a293a8b3e3b738416220e7fe1207d75201238ee"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/clamav-test/1.3.0/clamav-test-1.3.0-darwin-arm.tar.gz"
+      sha256 "960782a32ec71d04188efac58a10a4b81bbb4f4ce9b102f41fb5547d6bfd9419"
     end
   end
 
