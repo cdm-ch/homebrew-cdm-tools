68490437c36a090a79e7e94b6b220ac2b244663a
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-06-11 21:10:30 +0200

    Update nexus-auth to version 1.4.0

--- Formula/nexus-auth.rb
+++ Formula/nexus-auth.rb
@@ -1,15 +1,15 @@
 class NexusAuth < Formula
   desc "CDM tool: nexus-auth"
   homepage "https://github.com/cdm-ch"
-  version "1.3.0"
+  version "1.4.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/nexus-auth/1.3.0/nexus-auth-1.3.0-darwin-intel.tar.gz"
-      sha256 "6ad4a1919a0a690ce99e7129c2297075d13532c5a53d6c2219f0e0adba6a5cc8"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/nexus-auth/1.4.0/nexus-auth-1.4.0-darwin-intel.tar.gz"
+      sha256 "4e42a4a82c56d48d26c46e3d662adb139dd108320d734c17f0dc676e3877400f"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/nexus-auth/1.3.0/nexus-auth-1.3.0-darwin-arm.tar.gz"
-      sha256 "f2b41e626b6094a18b0e68d1ca1ffd887ac37b75ed12ff0f37a030eaee9207fe"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/nexus-auth/1.4.0/nexus-auth-1.4.0-darwin-arm.tar.gz"
+      sha256 "62ea41bc240fd5f5200d56f7512442501a283c75d929b17c244e4423e2164cd6"
     end
   end
 
