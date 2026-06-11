316e74228efeed9008d9fef5fb9a44b3cf478f54
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-06-11 21:11:01 +0200

    Update adc to version 1.22.0

--- Formula/adc.rb
+++ Formula/adc.rb
@@ -1,15 +1,15 @@
 class Adc < Formula
   desc "CDM tool: adc"
   homepage "https://github.com/cdm-ch"
-  version "1.21.0"
+  version "1.22.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.21.0/adc-1.21.0-darwin-intel.tar.gz"
-      sha256 "d145fff11e689073e2ee8e1a9a0ba149672e75b823ac8637f9ee3780d7385144"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.22.0/adc-1.22.0-darwin-intel.tar.gz"
+      sha256 "2873ac8acc6b1ce66d67209c5c6f86dcda6bbc1e3305da33f797fe64be045efb"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.21.0/adc-1.21.0-darwin-arm.tar.gz"
-      sha256 "c9ac3c12c2829d3780db77f446c53d7797d54ad2b19ca1a892c94c5eafa46826"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/adc/1.22.0/adc-1.22.0-darwin-arm.tar.gz"
+      sha256 "581ae9eb695fbd25be306a23975ef75c1ed13cdc1f645f2b7acbf26ebd9d87dd"
     end
   end
 
