165c3bdc6af5c0669bc37aae9b2a848c2eb65aa9
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-06-23 15:30:20 +0200

    Update smb-git-sync to version 1.5.0

--- Formula/smb-git-sync.rb
+++ Formula/smb-git-sync.rb
@@ -1,15 +1,15 @@
 class SmbGitSync < Formula
   desc "CDM tool: smb-git-sync"
   homepage "https://github.com/cdm-ch"
-  version "1.4.0"
+  version "1.5.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/smb-git-sync/1.4.0/smb-git-sync-1.4.0-darwin-intel.tar.gz"
-      sha256 "5b28515f6aea50bb271d049a9cd21340c67e387f4f3241920f4b51cccbadcdd5"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/smb-git-sync/1.5.0/smb-git-sync-1.5.0-darwin-intel.tar.gz"
+      sha256 "aeccee0428b1762e3d1286e67c79cae15690434acbed0e59b029edf317597195"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/smb-git-sync/1.4.0/smb-git-sync-1.4.0-darwin-arm.tar.gz"
-      sha256 "28a5dae487e042c9254e1a654b00b2b92af61bab487b084e250f0e1cf87a0977"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/smb-git-sync/1.5.0/smb-git-sync-1.5.0-darwin-arm.tar.gz"
+      sha256 "dcec8d18205d558f9f7ed1b7bed64b2791ad012f011636100169d96510860388"
     end
   end
 
