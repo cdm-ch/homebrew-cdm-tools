f4316717131f42dff0a37ed9822305284c1ce63b
MOOR, Arne <arne.moor@aerztekasse.ch>
2026-08-15 14:25:14 +0200

    Update version-compare to version 1.6.1

--- Formula/version-compare.rb
+++ Formula/version-compare.rb
@@ -1,15 +1,15 @@
 class VersionCompare < Formula
   desc "CDM tool: version-compare"
   homepage "https://github.com/cdm-ch"
-  version "1.6.0"
+  version "1.6.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.6.0/version-compare-1.6.0-darwin-intel.tar.gz"
-      sha256 "2015c9c6c4773d77cf0532de410173a8011f673bc0d3ea7f2688219e2f333340"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.6.1/version-compare-1.6.1-darwin-intel.tar.gz"
+      sha256 "4377e587acaa765c5cc204119f578c9a21ce3a8efdb64c4990d8e4a7823f50be"
     else
-      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.6.0/version-compare-1.6.0-darwin-arm.tar.gz"
-      sha256 "08889c180249f7c0c5fc6accb1cf2d99b1771b3ca9e56ee93ede7f8ce3921237"
+      url "https://nexus.cdm.smis.ch/repository/homebrew-binaries/version-compare/1.6.1/version-compare-1.6.1-darwin-arm.tar.gz"
+      sha256 "3d36bb9b8b0ea9d3fde497f46f75549a9954314064527693693885e6f3c0be6a"
     end
   end
 
